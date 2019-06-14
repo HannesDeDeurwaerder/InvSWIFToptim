70beebd561061e99be34b3558e5b04a562a94d63
HannesDeDeurwaerder <45042640+HannesDeDeurwaerder@users.noreply.github.com>
2019-06-14 11:56:24 +0200

    Update job.sh
    Make it work on my personal HPC account

--- Fig4_BiasOpitimizer/job.sh
+++ Fig4_BiasOpitimizer/job.sh
@@ -5,8 +5,9 @@
 #PBS -o R/log/SWIFT.o$PBS_JOBID
 #PBS -e R/log/SWIFT.e$PBS_JOBID
 
-ml R/3.4.4-intel-2018a-X11-20180131; ulimit -s unlimited
-cd /kyukon/data/gent/vo/000/gvo00074/felicien/R/InvSWIFToptim/Fig4_BiasOpitimizer
+ml R; ulimit -s unlimited
+cd /user/gent/408/vsc40883/InvSWIFToptim/Fig4_BiasOpitimizer
 
-echo "source('./R/run_parallel.r')" | R --vanilla
+
+echo "source('./R/run_parallel.r')" | R --safe
 
